5725229b748b5865b965333cccab2853a0f1f1c9
Tirzaan Neil <Tirzaan@tntneil.com>
2026-03-24 22:18:46 -0600

    Initial Commit

---
+++ CalledToCommunicate/CalledToCommunicateApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalledToCommunicateApp.swift
+//  CalledToCommunicate
+//
+//  Created by Tirzaan on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct CalledToCommunicateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
